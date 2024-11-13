4b5bb5f96a36694a06a71ea58a9b4ccd9f10de4d
soniya malviya <151515611+soniya-malviy@users.noreply.github.com>
2024-11-13 16:33:13 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/Header';
+import Header from '../Components/Header';
 import SpecialityMenu from "../Components/SpecialityMenu.jsx";
 import TopDoctors from "../Components/TopDoctors.jsx";
 import Banner from "../Components/Banner.jsx";
