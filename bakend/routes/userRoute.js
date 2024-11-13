1fa2ebfa3aede3349e5c59c8f730900ef4b0f42c
soniya malviya <soniya.04malviya@gmail.com>
2024-11-13 18:09:55 +0530

    done

--- bakend/routes/userRoute.js
+++ bakend/routes/userRoute.js
@@ -18,7 +18,7 @@ userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
 
 userRouter.get('/get-profile',authUser, getProfile)
-userRouter.put('/update-profile', upload.single('image'),authUser, updateProfile)
+userRouter.post('/update-profile', upload.single('image'),authUser, updateProfile)
 userRouter.post('/book-appointment', authUser, bookAppointment)
 userRouter.get('/appointments',authUser, listAppointment)
 userRouter.post('/cancel-appointment',authUser, cancelAppointments)
